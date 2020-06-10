966b2a6a5ec5a7187b4547a3040c296c4885a682
RaiMuhammadTayyab <66311583+RaiMuhammadTayyab@users.noreply.github.com>
2020-06-10 07:59:33 +0500

    Update App.js
    added hai

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
     <div className="App">
     
      <h1> Hello world from Tayyab</h1>
-     <h2>its very hard to make this app</h2>
+     <h2>its very hard to make this app
+       hai
+     </h2>
     
     </div>
   );
