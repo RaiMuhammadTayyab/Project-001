38ec36e647879651cabe6c374ae04fbe6889b919
RaiMuhammadTayyab <66311583+RaiMuhammadTayyab@users.noreply.github.com>
2020-06-10 06:46:09 +0500

    added paragraph indents
    changed the txt

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Hello world from Rai Muhammad Tayyab
+     <P> Hello world from Rai Muhammad Tayyab</p>
     </div>
   );
 }
