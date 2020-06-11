8e738b756059c3351a9d85c7a258d933971a45bb
RaiMuhammadTayyab <66311583+RaiMuhammadTayyab@users.noreply.github.com>
2020-06-11 23:48:43 +0500

    Update App.js
    chnages discription

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
      <h1> Hello world from Tayyab</h1>
      <h2>its very hard to make this app
        hai
-       but i am trying pls 
+       but i am trying pls hai every one i am coming 
      </h2>
     
     </div>
